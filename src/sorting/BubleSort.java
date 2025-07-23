35eafa6643f017cfd9c3fff157573e54771bf402
Aadit Antony <aaditantony1112@gmail.com>
2025-07-23 13:11:01 +0530

    bubble sort learned

---
+++ src/sorting/BubleSort.java
@@ -0,0 +1,28 @@
+package sorting;
+
+public class BubleSort {
+    static  void bubleSort(int []arr){
+        int n= arr.length;
+        boolean swapped;
+        for(int i=0;i<n-1;i++){
+            swapped=false;
+            for(int j=0;j<n-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    swapped=true;
+                }
+            }
+            if(!swapped) break;
+        }
+    }
+
+    public static void main(String[] args) {
+        int []arr={5,3,1,4,2};
+        bubleSort(arr);
+        for(int num:arr){
+            System.out.println(num+"");
+        }
+    }
+}
